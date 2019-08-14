20cc20f3804c6f3f576a307a6d1ff9267c5a48f1
lesliewy <leslie_wangyang@163.com>
2019-08-14 09:59:41 +0800

    添加elastic-job测试

--- myweb/src/main/java/com/wy/myweb/impl/RestImpl.java
+++ myweb/src/main/java/com/wy/myweb/impl/RestImpl.java
@@ -1,16 +1,18 @@
 package com.wy.myweb.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.wy.myweb.api.Rest;
 import com.wy.myweb.object.RestResult;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 /**
  * Created by leslie on 2018/3/12.
  */
 public class RestImpl implements Rest {
 
-    private static final Logger logger = LogManager.getLogger(RestImpl.class);
+    // private static final Logger logger = LogManager.getLogger(RestImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(RestImpl.class);
 
     @Override
     public RestResult query() {
